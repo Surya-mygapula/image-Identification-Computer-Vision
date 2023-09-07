3409b9870533fda3cc587b114794aa407a8ccf27
Surya-mygapula <138412414+Surya-mygapula@users.noreply.github.com>
2023-09-07 10:52:13 +0530

    Create Webcam image.py

---
+++ Webcam image.py
@@ -0,0 +1,54 @@
+def take_photo(filename='photo.jpg', quality=0.8):
+  js = Javascript('''
+    async function takePhoto(quality) {
+      const div = document.createElement('div');
+      const capture = document.createElement('button');
+      capture.textContent = 'Capture';
+      div.appendChild(capture);
+
+      const video = document.createElement('video');
+      video.style.display = 'block';
+      const stream = await navigator.mediaDevices.getUserMedia({video: true});
+
+      document.body.appendChild(div);
+      div.appendChild(video);
+      video.srcObject = stream;
+      await video.play();
+
+
+      google.colab.output.setIframeHeight(document.documentElement.scrollHeight, true);
+
+
+      await new Promise((resolve) => capture.onclick = resolve);
+
+      const canvas = document.createElement('canvas');
+      canvas.width = video.videoWidth;
+      canvas.height = video.videoHeight;
+      canvas.getContext('2d').drawImage(video, 0, 0);
+      stream.getVideoTracks()[0].stop();
+      div.remove();
+      return canvas.toDataURL('image/jpeg', quality);
+    }
+    ''')
+  display(js)
+
+
+  data = eval_js('takePhoto({})'.format(quality))
+
+  img = js_to_image(data)
+
+
+  detections, width_ratio, height_ratio = darknet_helper(img, width, height)
+
+
+  for label, confidence, bbox in detections:
+    left, top, right, bottom = bbox2points(bbox)
+    left, top, right, bottom = int(left * width_ratio), int(top * height_ratio), int(right * width_ratio), int(bottom * height_ratio)
+    cv2.rectangle(img, (left, top), (right, bottom), class_colors[label], 2)
+    cv2.putText(img, "{} [{:.2f}]".format(label, float(confidence)),
+                      (left, top - 5), cv2.FONT_HERSHEY_SIMPLEX, 0.5,
+                      class_colors[label], 2)
+
+  cv2.imwrite(filename, img)
+
+  return filename
